05b2a1c3091805a15f1e3eedcd64be37ec80cba9
tmcmanus95 <tmcmanus95@gmail.com>
2024-07-21 23:29:49 -0500

    bouquet rendering

--- client/src/utils/wordsToStemMatching.js
+++ client/src/utils/wordsToStemMatching.js
@@ -1,13 +1,13 @@
 export function wordsToStemMatching(wordAmount) {
   switch (wordAmount) {
     case 2:
-      return "./stemSprites/twoStem.png";
+      return "/stemSprites/twoStem.png";
     case 3:
-      return "./stemSprites/threeStem.png";
+      return "/stemSprites/threeStem.png";
     case 4:
-      return "./stemSprites/threeStem.png";
+      return "/stemSprites/threeStem.png";
 
     default:
-      return "./stemSprites/fiveStem.png";
+      return "/stemSprites/fiveStem.png";
   }
 }
