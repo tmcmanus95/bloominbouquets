c4f92e7c072d6f82bf0ce49dbfbe3f5e9ecca5e1
tmcmanus95 <tmcmanus95@gmail.com>
2024-07-28 23:43:26 -0500

    env

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,7 @@ const getDailyBoard = require("../utils/getDailyBoard");
 const shuffleCountToSeedReduction = require("../utils/shuffleCountToSeedReduction");
 const wordLengthToSeeds = require("../utils/wordLengthToSeeds");
 const { sendEmail } = require("../utils/sendEmail");
-const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const resolvers = {
   Query: {
