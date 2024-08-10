bd78b7e043869825df8e6cde10c82cd26a2c64ac
tmcmanus95 <tmcmanus95@gmail.com>
2024-08-09 23:18:13 -0500

    featured words component

--- client/src/components/FeaturedWords.jsx
+++ client/src/components/FeaturedWords.jsx
@@ -1,7 +1,15 @@
 export default function FeaturedWords() {
+  const featuredWords = ["First", "Featured", "Words", "Go", "Here"];
   return (
     <div>
       <div>Featured Words</div>
+      <div className="grid grid-cols-5">
+        {featuredWords.map((word, index) => (
+          <div className="bg-red border-green border-2 dark:text-white m-4 text-center">
+            {word}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
