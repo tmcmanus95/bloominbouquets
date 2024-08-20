c6fa20d7f1780b3792a7a697815c5fdd1da36b0c
tmcmanus95 <tmcmanus95@gmail.com>
2024-08-19 19:00:45 -0500

    shop page stylings

--- client/src/components/GameBoardMostRecentWordList.jsx
+++ client/src/components/GameBoardMostRecentWordList.jsx
@@ -3,7 +3,7 @@ export default function GameBoardMostRecentWordList({ words }) {
   let sortedWords = words?.slice(words.length - 10, words.length);
   sortedWords?.reverse();
   return (
-    <section className="ml-5 flex justify-center flex-col md:text-3xl text-2xl p-3  border-green-500 border-solid border-2">
+    <section className="ml-5 flex justify-center flex-col md:text-3xl text-2xl p-3  border-green-500 dark:border-white  border-solid border-2">
       <h1 className="md:text-3xl text-xl text-decoration-line: underline mb-2 text-center p-2">
         Recent Words
       </h1>
